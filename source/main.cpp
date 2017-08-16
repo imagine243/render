51f008510bbd8419f3bfd87eb5a056274ea812f8
超 张 <imagine243@gmail.com>
2017-08-16 22:12:32 +0800

    add sdl cmake

---
+++ source/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  render
+//
+//  Created by chao on 2017/8/7.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#include <iostream>
+#include "window.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
