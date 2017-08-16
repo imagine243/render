b21da0bc471579cc6a38d508953fc3d0d88c340c
张超 <imagine243>
2017-08-16 11:46:42 +0800

    fix window

--- source/window.h
+++ source/window.h
@@ -24,6 +24,15 @@ class Window {
     int _width;
     int _height;
     bool _runing;
+
+	bool pressedW = false;
+	bool pressedS = false;
+	bool pressedA = false;
+	bool pressedD = false;
+	bool pressedUp = false;
+	bool pressedDown = false;
+	bool pressedLeft = false;
+	bool pressedRight = false;
 public:
     Window(int argc, char * argv[], int width=WindowWidth, int height = WindowHeight, const char * title = "render"){
         //init 
@@ -44,12 +53,25 @@ public:
         _height = height;
 
         //canvas
+		_canvas = new Canvas((uint32_t)screen->pixels, width, height);
 
         _runing = true;
+
+		const char * modelPath = "";
+		const char * texturePath = "";
+
+		if (argc > 2) {
+			modelPath = argv[1];
+			texturePath = argv[2];
+		}
+
+		_mesh = new Mesh(modelPath, texturePath);
     };
 
     virtual ~Window(){
         //clean resouce
+		delete _canvas;
+		delete _mesh;
     };
 
     void run()
@@ -57,28 +79,105 @@ public:
         while(_runing){
             SDL_LockSurface(_screen);
         }
+
+		updateInput();
+		update();
+		clear();
+		draw();
+		show();
     };
 
     void update(){
+		static const float transform = 0.05;
 
+		_mesh->rotation.x += pressedUp ? transform : 0;
+		_mesh->rotation.x -= pressedDown ? transform : 0;
+		_mesh->rotation.y += pressedLeft ? transform : 0;
+		_mesh->rotation.y -= pressedRight ? transform : 0;
+
+		_mesh->position.y += pressedW ? transform : 0;
+		_mesh->position.y -= pressedS ? transform : 0;
+		_mesh->position.x -= pressedA ? transform : 0;
+		_mesh->position.x += pressedD ? transform : 0;
     };
 
     void updateInput()
     {
-
+		SDL_Event event;
+		while (SDL_PollEvent(&event))	
+		{
+			HandleEvent(&event);
+		}
     };
 
     void clear(){
-
+		_canvas->clear();
     };
 
     void draw(){
-
+		_canvas->drawMash(*_mesh);
     }
 
     void show(){
-        
+		SDL_UnlockSurface(_screen);
+		SDL_UpdateRect(_screen, 0, 0, 0, 0);
     }
+
+	void quit() {
+		_runing = false;
+	}
+
+	void onKeyEvent(const SDL_Event *event) {
+		SDLKey key = (*event).key.keysym.sym;
+		bool keyIsDown = (event->type == SDL_KEYDOWN);
+		if (key == SDLK_ESCAPE) {
+			quit();
+		}
+		else if (key == SDLK_UP) {
+			pressedUp = keyIsDown;
+		}
+		else if (key == SDLK_DOWN) {
+			pressedDown = keyIsDown;
+		}
+		else if (key == SDLK_LEFT) {
+			pressedLeft = keyIsDown;
+		}
+		else if (key == SDLK_RIGHT) {
+			pressedRight = keyIsDown;
+		}
+		else if (key == SDLK_w) {
+			pressedW = keyIsDown;
+		}
+		else if (key == SDLK_s) {
+			pressedS = keyIsDown;
+		}
+		else if (key == SDLK_a) {
+			pressedA = keyIsDown;
+		}
+		else if (key == SDLK_d) {
+			pressedD = keyIsDown;
+		}
+	}
+
+	void onMouseEvent(const SDL_Event *event) {
+
+	}
+	
+	void HandleEvent(const SDL_Event *event) {
+		unsigned char eventType = event->type;
+
+		if (eventType == SDL_QUIT) {
+			quit();
+		}
+		else if (eventType == SDL_KEYDOWN || eventType == SDL_KEYUP)
+		{
+			onKeyEvent(event);
+		}
+		else if(eventType == SDL_MOUSEBUTTONDOWN)
+		{
+			onMouseEvent(event);
+		}
+	}
 
 };
 #endif /* window_h */
