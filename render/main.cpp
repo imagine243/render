fd24943045bd9a903cbb566f39278b563ea23807
zhangc <zhangc@shanggame.com>
2017-08-18 17:27:38 +0800

    add sdl2 test in render/main.cpp

--- render/main.cpp
+++ render/main.cpp
@@ -8,8 +8,42 @@
 
 #include <iostream>
 
+#include <SDL.h>
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "sdl begin!\n";
+    int width = 640;
+    int height = 480;
+    uint32_t * pixels = new uint32_t[width * height];
+    
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            if (i > 300 && j > 300) {
+                pixels[j * width + i] = 0x00ff0000;
+            }
+        }
+    }
+    
+    SDL_Window * win = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_SWSURFACE);
+    SDL_Renderer * renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+//    SDL_Surface * surface = sdl_crea
+    SDL_Texture * texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+    
+    
+    SDL_UpdateTexture(texture, NULL, pixels, width * sizeof(uint32_t));
+    
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    
+    
+    SDL_Delay(3000);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    
     return 0;
 }
